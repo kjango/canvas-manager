1911b162e67c61a90eb0456a0d4edb92e5d18555
kjango@gmail.com <kjango@gmail.com@3dfde679-f362-68b5-5ed3-c219d2482568>
2014-02-05 21:22:29 +0000

    Criando os daos q faltam
    monga

--- CanvasCore/src/DAO/SituacaoDAO.java
+++ CanvasCore/src/DAO/SituacaoDAO.java
@@ -1,15 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package DAO;
 
+import Hibernate.Situacao;
+import Util.HibernateUtil;
+import org.hibernate.Session;
+
 /**
- *
- * @author Rosana
+ * Classe responsável pelas operações de Banco de Dados para os objetos do tipo Situacao
+ * @author Francisco
  */
+
 public class SituacaoDAO {
     
+    private Session session;
+    
+    /**
+     * Persiste um objeto Situacao na Base de Dados
+     * @param situacao objeto a ser persistido no Banco de Dados
+     */
+    public void save(Situacao situacao) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        
+        session.beginTransaction();
+        session.save(situacao);
+        session.getTransaction().commit();
+    }
+    
+    /**
+     * Altera um objeto Situacao na Base de Dados
+     * @param situacao objeto a ser alterado
+     */
+    public void update(Situacao situacao) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        
+        session.beginTransaction();
+        session.update(situacao);
+        session.getTransaction().commit();
+    }
+    
+    /**
+     * Remove da Base de Dados um objeto do tipo Situacao
+     * @param situacao objeto a ser removido do Banco de Dados
+     */
+    public void delete(Situacao situacao) {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        
+        session.beginTransaction();
+        session.delete(situacao);
+        session.getTransaction().commit();
+    }
 }
