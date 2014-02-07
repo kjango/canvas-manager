eabfff9a7a202e0104ac3cd3d3f1fcbc6d9d6fe1
kjango@gmail.com <kjango@gmail.com@3dfde679-f362-68b5-5ed3-c219d2482568>
2014-02-07 19:31:04 +0000

    JDocs criados

--- CanvasCore/src/Modelo/CadastroCM.java
+++ CanvasCore/src/Modelo/CadastroCM.java
@@ -4,30 +4,51 @@ import Hibernate.Credencial;
 import Hibernate.Usuario;
 
 /**
- *
+ * Classe da Camada de Modelo para cadastro de usuário
  * @author ROSANA
  */
 public class CadastroCM {
     private Usuario usuario;
     private Credencial credencial;
 
+    /**
+     * Construtor da classe
+     * @param usuario Objeto usuario
+     * @param credencial Objeto credencial
+     */
     public CadastroCM(Usuario usuario, Credencial credencial) {
         this.usuario = usuario;
         this.credencial = credencial;
     }
 
+    /**
+     * Método que retorna o objeto usuario
+     * @return Objeto usuario
+     */
     public Usuario getUsuario() {
         return usuario;
     }
 
+    /**
+     * Método que define o objeto usuario
+     * @param usuario Objeto usuario
+     */
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
 
+    /**
+     * Método que retorna o objeto credencial
+     * @return Objeto credencial
+     */
     public Credencial getCredencial() {
         return credencial;
     }
 
+    /**
+     * Método que define o objeto credencial
+     * @param credencial Objeto credencial
+     */
     public void setCredencial(Credencial credencial) {
         this.credencial = credencial;
     }
--- CanvasCore/src/Modelo/ListaProjetosCM.java
+++ CanvasCore/src/Modelo/ListaProjetosCM.java
@@ -5,7 +5,7 @@ import Hibernate.Projeto;
 import Hibernate.Usuario;
 
 /**
- *
+ * Classe da camada de modelo para a lista de projetos de um usuário
  * @author ROSANA
  */
 public class ListaProjetosCM {
@@ -13,32 +13,62 @@ public class ListaProjetosCM {
     private Projeto projetoAtivo;
     private Projeto projetoParte;
 
+    /**
+     * Construtor da classe
+     * @param usuario Objeto usuario
+     * @param projetoAtivo Objeto projeto que define qual o projeto ativo do usuario
+     * @param projetoParte Objeto projeto que define qual projoto tem o usuario como membro
+     */
     public ListaProjetosCM(Usuario usuario, Projeto projetoAtivo, Projeto projetoParte) {
         this.usuario = usuario;
         this.projetoAtivo = projetoAtivo;
         this.projetoParte = projetoParte;
     }
 
+    /**
+     * Método que retorna o objeto usuario
+     * @return Objeto usuario
+     */
     public Usuario getUsuario() {
         return usuario;
     }
 
+    /**
+     * Método que define o objeto usuario
+     * @param usuario Objeto usuario
+     */
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
 
+    /**
+     * Método que retorna o objeto projeto que seja o ativo para o usuario
+     * @return Objeto projeto que seja o ativo para o usuario
+     */
     public Projeto getProjetoAtivo() {
         return projetoAtivo;
     }
 
+    /**
+     * Método que define o objeto projeto que seja o ativo para o usuario
+     * @param projetoAtivo Objeto projeto que seja o ativo para o usuario
+     */
     public void setProjetoAtivo(Projeto projetoAtivo) {
         this.projetoAtivo = projetoAtivo;
     }
 
+    /**
+     * Método que retorna o objeto projeto que possua o usuario como membro
+     * @return Objeto projeto que possua o usuario como membro
+     */
     public Projeto getProjetoParte() {
         return projetoParte;
     }
 
+    /**
+     * Método que define o objeto projeto que possua o usuario como membro
+     * @param projetoParte Objeto projeto que possua o usuario como membro
+     */
     public void setProjetoParte(Projeto projetoParte) {
         this.projetoParte = projetoParte;
     }
