d7a78d0e40da632d6f8bbd9456d43633741927e7
rrogiski <rrogiski@3dfde679-f362-68b5-5ed3-c219d2482568>
2014-02-23 22:28:29 +0000

    Novo form de cadastro

---
+++ CanvasUser/src/java/Controller/RecuperaSenhaController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Rosana
+ */
+@ManagedBean(name="recupera")
+@ViewScoped
+public class RecuperaSenhaController {
+    
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
