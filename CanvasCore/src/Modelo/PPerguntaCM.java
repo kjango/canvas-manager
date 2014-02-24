6643f254ef71ae35524a8cabb56362c9dc9fc7fa
kjango@gmail.com <kjango@gmail.com@3dfde679-f362-68b5-5ed3-c219d2482568>
2014-02-24 16:34:18 +0000

    Juntei a PAvaliacao.java com a PPergunta.java. Fiz o mesmo pros CM delas

--- CanvasCore/src/Modelo/PPerguntaCM.java
+++ CanvasCore/src/Modelo/PPerguntaCM.java
@@ -4,12 +4,21 @@ public class PPerguntaCM {
 	private String pergunta;
 	private String resposta;
 	private String dica;
+//	private String nomeProjeto;
+//	private String autor;
+	private String avaliador;
+	private String comentario;
+	private double nota;
 	
-	public PPerguntaCM(String pergunta, String resposta, String dica) {
+	public PPerguntaCM(String pergunta, String resposta, String dica,
+			String avaliador, String comentario, double nota) {
 		super();
 		this.pergunta = pergunta;
 		this.resposta = resposta;
 		this.dica = dica;
+		this.avaliador = avaliador;
+		this.comentario = comentario;
+		this.nota = nota;
 	}
 
 	public String getPergunta() {
@@ -35,4 +44,30 @@ public class PPerguntaCM {
 	public void setDica(String dica) {
 		this.dica = dica;
 	}
+
+	public String getAvaliador() {
+		return avaliador;
+	}
+
+	public void setAvaliador(String avaliador) {
+		this.avaliador = avaliador;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	
+	
 }
