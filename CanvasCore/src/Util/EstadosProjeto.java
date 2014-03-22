90e1965dba12cc555f9f5d1fb88996a27d9c060c
kjango@gmail.com <kjango@gmail.com@3dfde679-f362-68b5-5ed3-c219d2482568>
2014-03-22 12:42:10 +0000

    Criei o enum dos estados mais algumas funções no UsuarioDAO

---
+++ CanvasCore/src/Util/EstadosProjeto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+/**
+ * ENUN com os possíveis status para um projeto
+ * @author kji
+ */
+public enum EstadosProjeto {
+    EM_PROCESSO,
+    AGUARDANDO_AVALIACAO,
+    AGUARDANDO_PARECER,
+    EMITINDO_PARECER,
+    APROVADO,
+    RECUSADO,
+    EXCLUIDO
+}
